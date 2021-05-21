f63ec52f9d341ca4ac085eec108998223dc6f2e3
ilanKanatenko <andromyth@walla.com>
2021-05-21 16:25:59 +0300

    change about content

--- pages/about.js
+++ pages/about.js
@@ -30,23 +30,19 @@ const About = () => {
       </Col>
       <Col md="6">
         <div className={`${createFadeInClass()}`}>
+          <p>My name is Ilan Kanatenko and I am an experienced software . </p>
           <p>
-            My name is Filip Jerga and I am an experienced software engineer and
-            freelance developer.{" "}
+            I have a Bachelor of Science degree in programing and experience
+            working on a wide range of technologies and projects in web
+            applications
           </p>
-          <p>
-            I have a Master's degree in Artificial Intelligence and several
-            years of experience working on a wide range of technologies and
-            projects from C++ development for ultrasound devices to modern
-            mobile and web applications in React and Angular.
-          </p>
-          <p>
+          {/* <p>
             Throughout my career, I have acquired advanced technical knowledge
             and the ability to explain programming topics clearly and in detail
             to a broad audience. I invite you to take my course, where I have
             put a lot of effort to explain web and software engineering concepts
             in a detailed, hands-on and understandable way.
-          </p>
+          </p> */}
         </div>
       </Col>
     </Row>
